a9ddc263a901562b6c6513a14d3ef6ff87f46ded
Mateo Freitas <mateofreitas027@gmail.com>
2024-02-27 19:18:33 -0300

    comit 27F

--- src/findOneByEmail.ts
+++ src/findOneByEmail.ts
@@ -1,11 +1,12 @@
 import { contactos } from "./contactos";
-function findOneByEmail(contacto:any) {
-    if (email === "") {
-        return contactos
-    }
-    return contactos.filter((contacto) => {
-        return contacto.email.includes(email)
-    })
-}
 
-export{findOneByEmail}
+function findOneByEmail(email: string) {
+
+    const encontrados = contactos.filter((contacto) => {
+        return contacto.email.includes(email)
+    }) 
+    return encontrados[0] 
+} 
+
+
+export { findOneByEmail }
