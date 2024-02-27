3213b0e536486e6e617c203ce7fe22e1a4bc48d7
Mateo Freitas <mateofreitas027@gmail.com>
2024-02-27 19:28:09 -0300

    commit final

--- src/updateIPAddress.ts
+++ src/updateIPAddress.ts
@@ -1,4 +1,12 @@
-function updateIPAddress(contacto:any) {
-    
+import { Contacto } from "./contactos";
+
+function updateIPAddress(contactos:Contacto[]) {
+    const actualizados = contactos.map((contacto) => {
+        return {
+            ...contacto,
+            ip_address: contacto.ip_address+"."+contacto.id
+        }
+    }) 
+    return actualizados
 }
 export{updateIPAddress}
